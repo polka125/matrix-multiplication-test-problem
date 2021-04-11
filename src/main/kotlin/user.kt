2f4e88a1120da2f481f3707f8ed97b37f9bee9ef
Sergey Novozhilov <novozhilov1999@mail.ru>
2021-04-11 03:12:26 +0300

    More tests. Added SmallMatrixMultiplier class

--- src/main/kotlin/user.kt
+++ src/main/kotlin/user.kt
@@ -1,14 +1,26 @@
 
 fun main() {
-    var mt2 = MatrixCreator.fromFlatArray(2, 4,
-        doubleArrayOf(
-            1.0,  5.0,  9.0,  12.0,
-            2.0,  6.0,  10.0, 14.0
-        )
-    )
-    println(mt2)
 
-    var mt3 = MatrixCreator.transpose(mt2)
-    println(mt2 * MatrixCreator.transpose(mt2))
+    val mt1 = MatrixCreator.fromRows(doubleArrayOf(1.0, 2.0, 3.0, 4.0, 5.0, 6.0))
+    val mt2 = MatrixCreator.transpose(mt1)
+    println(mt1)
+    println(mt2)
+    println(mt1 * mt2)
+    println(mt2 * mt1)
+    println(mt2 * mt1 * mt2)
+
+    // а теперь умножим что-нибудь большое
+
+
+//    var mt3 = MatrixCreator.fromFlatArray(2, 4,
+//        doubleArrayOf(
+//            1.0,  5.0,  9.0,  12.0,
+//            2.0,  6.0,  10.0, 14.0
+//        )
+//    )
+//    println(mt2)
+//
+//    var mt4 = MatrixCreator.transpose(mt2)
+//    println(mt2 * MatrixCreator.transpose(mt2))
 
 }
